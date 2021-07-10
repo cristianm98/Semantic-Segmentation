45b75407bcd99987d5677f7ebe121f129487e519
Cristian Miholca <cristimiholca@gmail.com>
2021-07-10 15:28:07 +0300

    debug #22

--- commons/utils.py
+++ commons/utils.py
@@ -133,8 +133,9 @@ def save_results(images, paths, predictions):
     print(images.shape)
     for idx, img in enumerate(images):
         new_img_path = os.path.join(args.results_dir, 'img_' + str(idx) + '.bmp')
-        img = img.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).type(torch.uint8).numpy()
-        pil_img = Image.fromarray(img)
+        # img = img.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).type(torch.uint8).numpy()
+        # pil_img = Image.fromarray(img)
+        pil_img = transforms.ToPILImage()(img)
         pil_img.save(str(idx) + '.bmp')
         # cv2.imwrite(str(new_img_path), np.transpose(img, (1, 2, 0)))
         # pil_img = transforms.ToPILImage()(img)
@@ -144,8 +145,9 @@ def save_results(images, paths, predictions):
         # torchvision.utils.save_image(img, new_img_path)
     for idx, img in enumerate(predictions):
         new_img_path = os.path.join(args.results_dir, 'pred_' + str(idx) + '.bmp')
-        img = img.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).type(torch.uint8).numpy()
-        pil_img = Image.fromarray(img)
+        pil_img = transforms.ToPILImage()(img)
+        # img = img.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).type(torch.uint8).numpy()
+        # pil_img = Image.fromarray(img)
         pil_img.save(str(idx) + '.bmp')
         # cv2.imwrite(str(new_img_path), np.transpose(img, (1, 2, 0)))
         # if not os.path.exists(new_img_path):
